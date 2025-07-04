477c57eff82729b39c5d99638c7dc8b87010e725
Beron Coran <69285139+catsriver@users.noreply.github.com>
2025-07-04 14:50:28 +0800

    Updated index.tsx

--- src/components/ExcelViewer/index.tsx
+++ src/components/ExcelViewer/index.tsx
@@ -558,29 +558,26 @@ const ExcelViewer: FC<ExcelViewerProps> = ({ file, onClearFile }) => {
                                                 )}
                                         </Flex>
 
-                                       {/* 功能筛选区 */}
-                                          <Flex>
-                                        	<CheckboxCards.Root defaultValue={["1"]} columns={{ initial: "1", sm: "3" }}>
-                                        		<CheckboxCards.Item value="1">
-                                        			<Flex direction="column" width="100%">
-                                        				<Text weight="bold">A1 Keyboard</Text>
-                                        				<Text>US Layout</Text>
-                                        			</Flex>
-                                        		</CheckboxCards.Item>
-                                        		<CheckboxCards.Item value="2">
-                                        			<Flex direction="column" width="100%">
-                                        				<Text weight="bold">Pro Mouse</Text>
-                                        				<Text>Zero-lag wireless</Text>
-                                        			</Flex>
-                                        		</CheckboxCards.Item>
-                                        		<CheckboxCards.Item value="3">
-                                        			<Flex direction="column" width="100%">
-                                        				<Text weight="bold">Lightning Mat</Text>
-                                        				<Text>Wireless charging</Text>
-                                        			</Flex>
-                                        		</CheckboxCards.Item>
-                                        	</CheckboxCards.Root>
-                                      </Flex>
+                                      {/* 筛选功能区 */}
+                                      <Flex>
+                                        <Flex>
+                                            <CheckboxGroup.Root
+                                                defaultValue={['1']}
+                                                name='example'
+                                            >
+                                                <CheckboxGroup.Item value='1'>
+                                                    脏污
+                                                </CheckboxGroup.Item>
+                                                <CheckboxGroup.Item value='2'>
+                                                    Serious
+                                                </CheckboxGroup.Item>
+                                                <CheckboxGroup.Item value='3'>
+                                                   划伤
+                                                </CheckboxGroup.Item>
+                                            </CheckboxGroup.Root>
+                                          </Flex>
+                                        <Flex></Flex>
+                                    </Flex>
 
                                         {/* 渲染表格数据 */}
                                         {renderTablePreview(sheet, previewMode)}
